089857b233f76d718f0b2cb111780a6a54062d77
Vincent <v3hodgins@gmail.com>
2025-11-03 20:14:13 -0500

    wipted

--- src/services/transactions.js
+++
@@ -1,6 +0,0 @@
-export const transactions = [
-  { id: 1, accountId: 1, type: "deposit", amount: 500, date: "2025-11-01" },
-  { id: 2, accountId: 1, type: "withdrawal", amount: 200, date: "2025-11-02" },
-  { id: 3, accountId: 2, type: "deposit", amount: 1000, date: "2025-10-30" },
-  { id: 4, accountId: 3, type: "crypto purchase", amount: 1.2, date: "2025-10-29" },
-];
